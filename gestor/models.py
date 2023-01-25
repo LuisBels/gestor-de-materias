a8c93d6946e9179d6c8f72e72501d14107f25374
LuisBels <luisbelr9@gmail.com>
2023-01-24 23:13:03 -0400

    creamos el primer template

--- gestor/models.py
+++ gestor/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Curso(models.Model):
+  codigo = models.CharField(primary_key=True, max_length=6)
+  nombre = models.CharField(max_length=100)
+  creditos = models.PositiveSmallIntegerField()
+
+  def __str__(self):
+    texto = "{0}: valor de ({1}) creditos"
+    return texto.format(self.nombre, self.creditos,)
--- gestor/views.py
+++ gestor/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Curso
 
 # Create your views here.
+def home(request):
+  cursos = Curso.objects.all()
+  return render(request, "gestionC.html",{
+    "cursos":cursos
+  })
