fb7a95a52fe2450fa36aecc3a82d9a4f188229a3
LuisBels <luisbelr9@gmail.com>
2023-01-28 02:09:40 -0400

    agregamos los archivos staticos, terminamos la version 1.0

--- gestor/urls.py
+++ gestor/urls.py
@@ -5,5 +5,7 @@ from django.urls import path
 urlpatterns = [
     path("", views.home,name="home"),
     path("registroCurso/",views.registroCurso),
+    path("edicionCurso/<codigo>", views.edicionCurso),
+    path("editarCurso/", views.editarCurso),
     path("eliminacionCurso/<codigo>",views.eliminacionCurso)
 ]
--- gestor/views.py
+++ gestor/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Curso
 
+
 # Create your views here.
 def home(request):
   cursos = Curso.objects.all()
+ 
   return render(request, "gestionC.html",{
     "cursos":cursos
   })
@@ -17,9 +19,29 @@ def registroCurso(request):
 
   return redirect('/')
 
+def edicionCurso(request, codigo):
+  curso = Curso.objects.get(codigo=codigo)
+ 
+  return render(request, "edicionC.html",{"curso":curso})
+
+
+def editarCurso(request):
+  codigo = request.POST['txtCodigo']
+  nombres = request.POST['txtNombre']
+  creditos = request.POST['numCredito']
+
+  curso = Curso.objects.get(codigo=codigo)
+
+  curso.nombre = nombres
+  curso.creditos = creditos
+  curso.save()
+  
+
+  return redirect('/')
 
 def eliminacionCurso(request, codigo):
   curso = Curso.objects.get(codigo=codigo)
   curso.delete()
+  
 
   return redirect('/')
